cc49d435c11a1cddedc96ad69f038bdc1651efd5
tony-g30 <tonyaudo@gmail.com>
2024-07-23 07:15:19 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+  ];
+  
+  function superbowlWin(record) {
+    const winningYear = record.find(game => game.result === "W");
+    return winningYear ? winningYear.year : undefined;
+  }  
